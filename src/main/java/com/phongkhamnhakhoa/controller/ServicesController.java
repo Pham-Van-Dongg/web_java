96a2f27559353a93a03f7da77df344ad517e3f60
KimThoa2504 <tkthoa0425@gmail.com>
2025-01-08 17:09:29 +0700

    Update ServicesController.java

--- src/main/java/com/phongkhamnhakhoa/controller/ServicesController.java
+++ src/main/java/com/phongkhamnhakhoa/controller/ServicesController.java
@@ -46,16 +46,7 @@ public class ServicesController {
 		return "redirect:/services"; // Quay về trang danh sách dịch vụ
 	}
 
-	// Xóa dịch vụ
-//    @DeleteMapping("/delete/{id}")
-//    public String deleteService(@PathVariable Long id) {
-//        try {
-//            servicesService.deleteServiceById(id);
-//            return "redirect:/services"; // Quay lại danh sách dịch vụ sau khi xóa
-//        } catch (RuntimeException e) {
-//            return "redirect:/services"; // Nếu có lỗi, quay lại danh sách
-//        }
-//    }
+	
 	// Xóa dịch vụ
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<String> deleteService(@PathVariable Long id) {
@@ -67,19 +58,6 @@ public class ServicesController {
 					.body("Không thể xóa dịch vụ: " + e.getMessage()); // Trả về thông điệp lỗi
 		}
 	}
-
-//    @DeleteMapping("/delete/{id}")
-//    public String deleteService(@PathVariable Long id, Model model) {
-//        try {
-//            servicesService.deleteServiceById(id); 
-//            model.addAttribute("message", "Xóa dịch vụ thành công!");
-//            return "redirect:/services"; // Trở lại danh sách dịch vụ
-//        } catch (RuntimeException e) {
-//        	 model.addAttribute("errorMessage", "Không thể xóa dịch vụ: " + e.getMessage());
-//            // Xử lý lỗi nếu cần, chẳng hạn ghi log hoặc hiển thị thông báo
-//            return "redirect:/services"; // Quay về trang danh sách dịch vụ
-//        }
-//    }
 
 	// Tìm kiếm dịch vụ theo tên
 	@GetMapping("/search")
